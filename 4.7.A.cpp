6529c342e710141adbfcf10596e0be018d87b4b0
skalem <pimako2003@yandex.by>
2021-10-08 11:21:57 +0300

    Made input, and function strchr

--- 4.7.A.cpp
+++ 4.7.A.cpp
@@ -2,7 +2,32 @@
 
 using namespace std;
 
+
+char * strchr(char* string, int symbol){
+
+    int i = 0;
+     do {
+        if (*(string + i) == symbol){
+            return string + i;
+        }
+        ++i;
+    }while (string[i - 1] != '\0');
+    return nullptr;
+}
+
+
 int main(){
+    int amount;
+    char symbol;
+    cout << "Enter amount of elements in the string: ";
+    cin >> amount;
+    char* string = new char[amount + 1];
+    cout << "Enter the string: ";
+    cin >> string;
+    cout << "Enter the symbol, which you want to find: ";
+    cin >> symbol;
+    cout << *strchr(string, symbol);
 
 }
+
 
