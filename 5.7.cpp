ab370954581fc884c2dfe0718998766d9939b1bf
skalem <pimako2003@yandex.by>
2021-10-17 15:11:08 +0300

    Program now is able to find equal strings and output their amount, first line where they occurred and the string

--- 5.7.cpp
+++ 5.7.cpp
@@ -5,15 +5,8 @@
 
 using namespace std;
 
-bool compare(string line1, string line2) {
-    if (line1 == line2) {
-        return true;
-    }
-    return false;
-}
-
 char *takePath() {
-    char *path = new char[50];
+    char *path = new char[100];
     cout << "Enter the address to the file" << endl;
     cin >> path;
     ifstream reader(path);
@@ -46,13 +39,64 @@ vector<string> read(char *path) {
     return text;
 }
 
+vector<vector<int>> equalStrings(vector<string> text){
+    int equalAmount = 1;
+    vector<vector<int>> IndexAmount;
+    for (int i = 1; i < text.size(); ++i) {
+        if (text[i] == text[i - 1]){
+            int j = i;
+            vector<int> temp;
+            temp.push_back(i);
+                while (text[j] == text[j - 1]) {
+                    ++equalAmount;
+                    ++j;
+                }
+                i = j;
+            temp.push_back(equalAmount);
+            IndexAmount.push_back(temp);
+            equalAmount = 1;
+        }
+    }
+    return IndexAmount;
+}
+
+char* pathToPackage(const char* path){
+    int i = 0, j = 0;
+    while (path[i] != '\0'){
+        ++i;
+    }
+    char* pathPackage = new char[i + 1];
+    for (int k = i; path[k - 1] != '/'; --k) {
+        ++j;
+    }
+
+    i -= j;
+    for (int k = 0; k < i; ++k) {
+        pathPackage[k] = path[k];
+    }
+    pathPackage[i + 1] = '\0';
+    return pathPackage;
+}
+
+void output(const vector<vector<int>>& IndexAmount, vector<string> text,char* pathPackage){
+    string file = "output.txt";
+    string path = pathPackage + file;
+    ofstream fout(path);
+    for (int i = 0; i < IndexAmount.size(); ++i) {
+        int index = IndexAmount[i][0];
+        int amount = IndexAmount[i][1];
+        string line = text[index];
+        fout << "Line \"" << line << "\" starting from line " << index << " appeared to repeat " << amount << " times" << endl;
+    }
+    cout << "The file lies in: " << path;
+}
+
 int main() {
     char *path;
     path = takePath();
     vector<string> text = read(path);
-
-
-
-
+    vector<vector<int>> IndexAmount = equalStrings(text);
+    char *pathPackage = pathToPackage(path);
+    output(IndexAmount, text, pathPackage);
     return 0;
 }
