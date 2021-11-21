a728f3d203ed6f6064f59267ebe85f693de1af75
skalem <pimako2003@yandex.by>
2021-11-21 17:02:54 +0300

    Made class StudentAfter1Session

--- 7Lab/StudentAfter1Session.cpp
+++ 7Lab/StudentAfter1Session.cpp
@@ -3,3 +3,44 @@
 //
 
 #include "StudentAfter1Session.h"
+
+StudentAfter1Session::StudentAfter1Session(int studentNum, char *name, int curs1, int group, const int *marks) : student(studentNum, name, curs1, group){
+    for (int i = 0; i < 4; ++i) {
+        this->marks[i] = marks[i];
+    }
+}
+
+StudentAfter1Session::StudentAfter1Session(StudentAfter1Session *exClass) : student(exClass->studentNum, exClass->name,
+                                                                                    exClass->curs, exClass->group) {
+    for (int i = 0; i < 4; ++i) {
+        this->marks[i] = exClass->marks[i];
+    }
+}
+
+void StudentAfter1Session::setMarks(int* marks) {
+    for (int i = 0; i < 4; ++i) {
+        this->marks[i] = marks[i];
+    }
+}
+
+int* StudentAfter1Session::getMarks() {
+    return marks;
+}
+
+ostream &operator<<(ostream &out, const StudentAfter1Session &classEx) {
+    out << "Student number: " << classEx.studentNum << endl;
+    out << "Name: " << classEx.name << endl;
+    out << "Curs: " << classEx.curs << endl;
+    out << "Group: " << classEx.group << endl;
+    out << "Marks: " << classEx.marks << endl;
+    out << "UUID: " << classEx.UUID << endl;
+    return out;
+}
+
+int StudentAfter1Session::getAverageMark() {
+    int sum = 0;
+    for (int mark : this->marks) {
+        sum += mark;
+    }
+    return sum / 4;
+}
--- 7Lab/StudentAfter1Session.h
+++ 7Lab/StudentAfter1Session.h
@@ -1,12 +1,23 @@
-//
-// Created by skalem on 19.11.21.
-//
-
 #ifndef INC_7LAB_STUDENTAFTER1SESSION_H
 #define INC_7LAB_STUDENTAFTER1SESSION_H
 
+#include "student.h"
 
-class StudentAfter1Session {
+class StudentAfter1Session : public student {
+protected:
+    int marks[4];
+public:
+    StudentAfter1Session(int studentNum, char *name, int curs1, int group, const int marks[4]);
+
+    StudentAfter1Session(StudentAfter1Session *exClass);
+
+    void setMarks(int *marks);
+
+    int *getMarks();
+
+    friend ostream &operator<<(ostream &out, const StudentAfter1Session &classEx);
+
+    virtual int getAverageMark();
 
 };
 
