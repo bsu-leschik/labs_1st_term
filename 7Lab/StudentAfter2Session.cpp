93dd76605019a19780261f98118db8b766f01c85
skalem <pimako2003@yandex.by>
2021-11-21 17:23:24 +0300

    Made class StudentAfter2Session

--- 7Lab/StudentAfter2Session.cpp
+++ 7Lab/StudentAfter2Session.cpp
@@ -3,3 +3,48 @@
 //
 
 #include "StudentAfter2Session.h"
+
+StudentAfter2Session::StudentAfter2Session(int studentNum, char *name, int curs1, int group, const int *marks,
+                                           const int *marks2Session) : StudentAfter1Session(studentNum, name, curs1, group, marks){
+    for (int i = 0; i < 5; ++i) {
+        this->marks2Session[i] = marks2Session[i];
+    }
+}
+
+StudentAfter2Session::StudentAfter2Session(StudentAfter2Session *exClass) : StudentAfter1Session(exClass){
+    for (int i = 0; i < 5; ++i) {
+        this->marks2Session[i] = marks2Session[i];
+    }
+}
+
+void StudentAfter2Session::set2SessionMarks(int *marks) {
+    for (int i = 0; i < 5; ++i) {
+        this->marks2Session[i] = marks[i];
+    }
+}
+
+int *StudentAfter2Session::get2SessionMarks() {
+    return marks2Session;
+}
+
+int StudentAfter2Session::getAverageMark() {
+    int sum = 0;
+    for (int mark : this->marks2Session) {
+        sum += mark;
+    }
+    for (int mark : this->marks) {
+        sum += mark;
+    }
+    return sum / 9;
+}
+
+ostream &operator<<(ostream &out, const StudentAfter2Session &classEx) {
+    out << "Student number: " << classEx.studentNum << endl;
+    out << "Name: " << classEx.name << endl;
+    out << "Curs: " << classEx.curs << endl;
+    out << "Group: " << classEx.group << endl;
+    out << "Marks after 1st session: " << classEx.marks << endl;
+    out << "Marks after 2nd session: " << classEx.marks2Session << endl;
+    out << "UUID: " << classEx.UUID << endl;
+    return out;
+}
--- 7Lab/StudentAfter2Session.h
+++ 7Lab/StudentAfter2Session.h
@@ -5,8 +5,25 @@
 #ifndef INC_7LAB_STUDENTAFTER2SESSION_H
 #define INC_7LAB_STUDENTAFTER2SESSION_H
 
+#include "StudentAfter1Session.h"
 
-class StudentAfter2Session {
+class StudentAfter2Session : StudentAfter1Session {
+
+protected:
+    int marks2Session[5];
+
+    StudentAfter2Session(int studentNum, char *name, int curs1, int group, const int marks[4],
+                         const int marks2Session[5]);
+
+    StudentAfter2Session(StudentAfter2Session *exClass);
+
+    void set2SessionMarks(int *marks);
+
+    int *get2SessionMarks();
+
+    int getAverageMark() override;
+
+    friend ostream &operator<<(ostream &out, const StudentAfter2Session &classEx);
 
 };
 
