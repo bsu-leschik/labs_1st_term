6fea866fe83388d4261a6ea7afcf11c6827f8e44
skalem <pimako2003@yandex.by>
2021-11-30 23:58:14 +0300

    Made tests for functions

--- 7Lab/tests.cpp
+++ 7Lab/tests.cpp
@@ -1,6 +1,7 @@
 #include "StudentAfter2Session.h"
 #include "gtest/gtest.h"
 #include "string"
+#include "functions.h"
 
 string name = "aboba";
 int marks1[4] = {1, 3, 5, 7};
@@ -47,3 +48,19 @@ TEST(StudentAfter2Session, getAverageMark){
     ASSERT_EQ(studentAfter2Session.getAverageMark(), 7);
 }
 
+TEST(Functions, groupAvgAfter1Session){
+    vector<StudentAfter1Session> students;
+    students.push_back(&studentAfter1Session);
+    students.push_back(&studentAfter2Session);
+
+    ASSERT_EQ(groupAvgAfter1Session(students, 4), 4);
+}
+
+TEST(Functions, groupAvgAfter2Session){
+    vector<StudentAfter2Session> students;
+    int marks3[5] = {10, 7, 9, 0, 8};
+    StudentAfter2Session studentA2(25, name, 5, 1, marks1, marks3);
+    students.push_back(&studentAfter2Session);
+
+    ASSERT_EQ(groupAvgAfter2Session(students, 4), 7);
+}
